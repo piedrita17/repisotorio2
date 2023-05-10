3622a4ff9f563a3573179dca1382f2a007c5661a
piedrita17 <132463359+piedrita17@users.noreply.github.com>
2023-05-10 13:02:48 -0300

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,6 @@
+var numero1=prompt("ingresa primer numero");
+var numero2=prompt("ingresa segundo umero");
+var resultado=parseInt(numero1) + parseInt(numero2);
+console.log(resultado);
+alert(resultado);
+//comentario
